df3ae348d6cba41126af4ae3465c3fcb2a152718
Milan Felix Šulc <rkfelix@gmail.com>
2017-07-31 18:13:12 +0200

    DebugAuthenticator: use given identity

--- src/Auth/DebugAuthenticator.php
+++ src/Auth/DebugAuthenticator.php
@@ -44,6 +44,8 @@ class DebugAuthenticator implements IAuthenticator
 			throw new AuthenticationException('Cannot login', IAuthenticator::FAILURE);
 		}
 
+		if (!$this->identity) return $this->identity;
+
 		return new Identity(1, NULL, NULL);
 	}
 
